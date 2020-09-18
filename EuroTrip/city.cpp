f19132931306b19bc959b9b8122f999101694b6f
Purdy <pu12dy@yahoo.com>
2020-09-18 13:25:55 -0700

    Changed Class Citys Defualt Construtor
    Romoved pretzels

--- EuroTrip/city.cpp
+++ EuroTrip/city.cpp
@@ -4,11 +4,6 @@ City::City()
 {
     cityName = "Berlin";
     QPair<QString, double> input;
-    input.first = "Pretzels";
-    input.second = 4.00; //The price of the food
-
-    foodInfo.push_front(input);
-
 }
 
 City::City(QString name, QString foodName, double foodCost)
